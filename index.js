04a712b47adc98e735dfa6d55b10cfae5251938f
MacioBBS <marciobbs@gmail.com>
2021-04-18 13:44:51 -0700

    Add return dispatch

--- index.js
+++ index.js
@@ -14,13 +14,14 @@ function createStore() {
   // 4. Update the state
 
   let state;
-  let listeners = [];
+  let listeners = []; // Array of functions
 
   // 1. The State
   const getState = () => state;
 
   /**
    * 3. Listen to changes on the state
+   * Takes in functions that will be called when the state changes
    * @param {function} listener
    * @returns function to unsubscribe
    */
@@ -36,11 +37,13 @@ function createStore() {
   // Responsible to update the sate inside of the actual Store - It needs to receive the Actiion to tell dispatch() the specific event that occurred inside of the applicaton
   const dispatch = action => {
     state = toDos(state, action);
+    listeners.forEach(listener => listener()); // Invoke each function inside of the array.
   };
 
   return {
     getState,
     subscribe,
+    dispatch,
   };
 }
 

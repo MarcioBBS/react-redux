92f0023f77574d89d3c0da7153284be631d7b645
MacioBBS <marciobbs@gmail.com>
2021-04-19 22:05:00 -0700

    Create Goal reducer pure function

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function createStore(reducer) {
   };
 }
 
-// APP CODE - Reducer
+// APP To do - Reducer
 function toDos(state = [], action) {
   switch (action.type) {
     case "ADD_TODO":
@@ -54,6 +54,18 @@ function toDos(state = [], action) {
       return state.filter(todo => todo.id !== action.id);
     case "TOGGLE_TODO":
       return state.map(todo => (todo.id !== action.id ? todo : Object.assign({}, todo, { complete: !todo.complete })));
+    default:
+      return state;
+  }
+}
+
+// App Goals - Reducer
+function Goals(state = [], action) {
+  switch (action.type) {
+    case "ADD_GOAL":
+      return state.concat([action.goal]);
+    case "REMOVE_GOAL":
+      return state.filter(goal => goal.id !== action.id);
     default:
       return state;
   }
